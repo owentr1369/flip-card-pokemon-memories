857fbe15a93b42fda134bb2f6610036df1892bad
owentr1369 <tamvaa1306@gmail.com>
2022-05-10 09:57:53 +0700

    build base

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Hello</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
